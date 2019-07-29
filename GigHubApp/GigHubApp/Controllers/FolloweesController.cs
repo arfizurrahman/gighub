bc57d5eb881f09265903e82e77682918e917f4ef
Arfizur Rahman <arfizrahman0@gmail.com>
2019-07-29 19:49:58 +0600

    Implement artist's I'm following

---
+++ GigHubApp/GigHubApp/Controllers/FolloweesController.cs
@@ -0,0 +1,30 @@
+﻿using GigHubApp.Models;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHubApp.Controllers
+{
+    public class FolloweesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public FolloweesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Followees
+        public ActionResult Following()
+        {
+            var userId = User.Identity.GetUserId();
+            List<Following> followees = _context.Followings
+                .Where(f => f.FollowerId == userId)
+                .Include(f => f.Followee)
+                .ToList();
+
+            return View(followees);
+        }
+    }
+}
---
+++ GigHubApp/GigHubApp/ViewModels/FollowingViewModel.cs
@@ -0,0 +1,11 @@
+﻿using GigHubApp.Models;
+using System.Collections.Generic;
+
+namespace GigHubApp.ViewModels
+{
+    public class FollowingViewModel
+    {
+        public IEnumerable<Following> Followings { get; set; }
+
+    }
+}
