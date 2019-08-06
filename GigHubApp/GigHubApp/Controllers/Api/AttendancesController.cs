832e17654f8a7a586403f3abe200c39e16929d54
Arfizur Rahman <arfizrahman0@gmail.com>
2019-08-06 19:47:43 +0600

    Implement gig attendance delete

--- GigHubApp/GigHubApp/Controllers/Api/AttendancesController.cs
+++ GigHubApp/GigHubApp/Controllers/Api/AttendancesController.cs
@@ -1,8 +1,8 @@
-﻿using System.Linq;
-using System.Web.Http;
-using GigHubApp.Dtos;
+﻿using GigHubApp.Dtos;
 using GigHubApp.Models;
 using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
 
 namespace GigHubApp.Controllers.Api
 {
@@ -36,6 +36,23 @@ namespace GigHubApp.Controllers.Api
             _context.SaveChanges();
 
             return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteAttendance(int id)
+        {
+            var userId = User.Identity.GetUserId();
+
+            var attendance = _context.Attendances
+                .SingleOrDefault(a => a.AttendeeId == userId && a.GigId == id);
+
+            if (attendance == null)
+                return NotFound();
+
+            _context.Attendances.Remove(attendance);
+            _context.SaveChanges();
+
+            return Ok(id);
         }
     }
 }
