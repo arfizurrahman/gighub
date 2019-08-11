dcdcb7911d4c7bfa8761a8a093a38c519ffd61bb
Arfizur Rahman <arfizrahman0@gmail.com>
2019-08-11 15:04:21 +0600

    Configure integration test project

--- GigHubApp/GigHub.IntegrationTests/GlobalSetUp.cs
+++ GigHubApp/GigHub.IntegrationTests/GlobalSetUp.cs
@@ -1,5 +1,8 @@
-﻿using NUnit.Framework;
+﻿using GigHubApp.Core.Models;
+using GigHubApp.Persistence;
+using NUnit.Framework;
 using System.Data.Entity.Migrations;
+using System.Linq;
 
 namespace GigHub.IntegrationTests
 {
@@ -9,9 +12,28 @@ namespace GigHub.IntegrationTests
         [SetUp]
         public void SetUp()
         {
+            MigrateDbToLatestVersion();
+            Seed();
+        }
+
+        private static void MigrateDbToLatestVersion()
+        {
             var configuration = new GigHubApp.Migrations.Configuration();
             var migrator = new DbMigrator(configuration);
             migrator.Update();
+        }
+
+        public void Seed()
+        {
+            var context = new ApplicationDbContext();
+
+            if (context.Users.Any())
+                return;
+
+            context.Users.Add(new ApplicationUser { UserName = "user1", Name = "user1", Email = "-", PasswordHash = "-" });
+            context.Users.Add(new ApplicationUser { UserName = "user2", Name = "user2", Email = "-", PasswordHash = "-" });
+
+            context.SaveChanges();
         }
     }
 }
